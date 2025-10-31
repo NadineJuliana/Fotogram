2c56f665b973ee4128b31a4818b1203eb510daae
NadineJuliana <nadine.juliana.b@web.de>
2025-10-31 15:14:08 +0100

    add forward an backward function

--- js/script.js
+++ js/script.js
@@ -39,10 +39,16 @@ const DIO_IMAGES_TITLES = [
 ]
 
 // global Variables
-const DIALOG_TITLES = document.getElementById('HeaderDialog')
-const ARROW_BACKWARD = document.getElementById('ArrowLeft')
-const ARROW_FORWARD = document.getElementById('ArrowRight')
-const DIALOG_COUNTER = document.getElementById('DialogCounter')
+const DIALOG_REF = document.getElementById('DialogShowImg');
+const DIALOG_TITLES = document.getElementById('HeaderDialogRef');
+const CLOSE_HEADER = document.getElementById('HeaderCloseButton');
+// let HEADER_REF = document.getElementById('HeaderDialog');
+const ARROW_BACKWARD = document.getElementById('ArrowLeft');
+const ARROW_FORWARD = document.getElementById('ArrowRight');
+const DIALOG_COUNTER = document.getElementById('DialogCounter');
+
+let ACTUAL_INDEX = 0;
+
 
 
 // funtions
@@ -50,24 +56,59 @@ function renderImages(){
     const IMG_REF = document.getElementById('DioGallery');
 
     for (let c= 0; c < DIO_IMAGES.length; c++){
-        IMG_REF.innerHTML += `<img id="DioImages" onclick="openDialog(${c})" src="${DIO_IMAGES[c]}" alt="">`;
+        IMG_REF.innerHTML += `<img id="DioImages" aria-haspopup="dialog" aria-controls="DialogImg" onclick="openDialog(${c})" src="${DIO_IMAGES[c]}" alt="">`;
     }
 }
 
 
-const DIALOG_REF = document.getElementById('DialogShowImg');
-
 function openDialog(listindex) {
-    const DIALOG_IMG_REF = document.getElementById('DialogImg')
-    DIALOG_IMG_REF.innerHTML = `<img id="BigImg${listindex}" src="${DIO_IMAGES[listindex]}" alt="">`
+    const DIALOG_IMG_REF = document.getElementById('DialogImg');
+    DIALOG_IMG_REF.innerHTML = `<img id="BigImg" src="${DIO_IMAGES[listindex]}" alt="">`;
+    DIALOG_TITLES.innerHTML = `<h2>${DIO_IMAGES_TITLES[listindex]}</h2>`;
+
+    // HEADER_REF.innerHTML = `${DIALOG_TITLES} ${CLOSE_HEADER}`;
+    
+    ARROW_BACKWARD.innerHTML = `<img class="DialogNavButton" onclick="goBackward()" src="img/arrow-left.png" alt="Arrow Left">`;
+    DIALOG_COUNTER.innerHTML = ` <p id="CounterDialog">${listindex +1} /${DIO_IMAGES.length}</p>`;
+    ARROW_FORWARD.innerHTML = `<img class="DialogNavButton" onclick="goForward()" src="img/arrow-right.png" alt="Arrow Right">`;
+    
+    ACTUAL_INDEX = listindex;
     DIALOG_REF.showModal();
-    DIALOG_TITLES.innerHTML += `<h2>${DIO_IMAGES_TITLES[listindex]}</h2>`;
-    ARROW_BACKWARD.innerHTML = `<img class="DialogNavButton" src="img/arrow-left.png" alt="Arrow Left">`;
-    DIALOG_COUNTER.innerHTML = ` <p>${listindex +1} /${DIO_IMAGES.length}</p>`;
-    ARROW_FORWARD.innerHTML = `<img class="DialogNavButton" src="img/arrow-right.png" alt="Arrow Right">`;
 }
+
+
+// function renderHeader(){
+//     DIALOG_TITLES.innerHTML = `<h2>${DIO_IMAGES_TITLES[ACTUAL_INDEX]}</h2>`;
+//     CLOSE_HEADER.innerHTML = `<img class="DialogCloseButton" onclick="closeDialog()" src="img/close.png" alt="Close Button">`;
+// }
 
 function closeDialog(){
     DIALOG_REF.close();
 }
 
+
+function goForward(){
+    if (ACTUAL_INDEX +1 < DIO_IMAGES.length){
+        ACTUAL_INDEX++; 
+    } else {
+        ACTUAL_INDEX = 0;
+    }
+    DIALOG_TITLES.innerHTML = `<h2>${DIO_IMAGES_TITLES[ACTUAL_INDEX]}</h2>`;
+    const TEXT_DIALOG = document.getElementById('CounterDialog');
+    TEXT_DIALOG.innerHTML = `${ACTUAL_INDEX +1} /${DIO_IMAGES.length}`;
+    const DIALOG_IMG_REF = document.getElementById('BigImg');
+    DIALOG_IMG_REF.setAttribute('src', `${DIO_IMAGES[ACTUAL_INDEX]}`);
+}
+
+function goBackward(){
+    if (ACTUAL_INDEX > 0) {
+        ACTUAL_INDEX --;
+    } else {
+        ACTUAL_INDEX = DIO_IMAGES.length -1;
+    }
+    DIALOG_TITLES.innerHTML = `<h2>${DIO_IMAGES_TITLES[ACTUAL_INDEX]}</h2>`;
+    const TEXT_DIALOG = document.getElementById('CounterDialog');
+    TEXT_DIALOG.innerHTML = `${ACTUAL_INDEX +1} / ${DIO_IMAGES.length}`;
+    const DIALOG_IMG_REF = document.getElementById('BigImg');
+    DIALOG_IMG_REF.setAttribute('src', `${DIO_IMAGES[ACTUAL_INDEX]}`);
+}
